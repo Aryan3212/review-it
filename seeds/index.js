30e4b7f4e24327782028a77603490b59e50c8dc0
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-09-18 13:20:46 +0600

    feat: seeding script

---
+++ seeds/index.js
@@ -0,0 +1,39 @@
+const { CampgroundModel } = require("../models/campgroundModel");
+const { db } = require("../db");
+
+async function seed() {
+    const camps = require("./cities");
+    const { places, descriptors } = require("./titleDescriptors");
+    const documents = [];
+
+    await CampgroundModel.deleteMany({});
+    camps.map((c) => {
+        const camp = new CampgroundModel({
+            title:
+                descriptors[Math.floor(Math.random() * descriptors.length)] +
+                " " +
+                places[Math.floor(Math.random() * places.length)],
+            price: Math.random() * 100,
+            description: "This is the description!",
+            location: {
+                geometry: {
+                    coordinates: [c.longitude, c.latitude],
+                },
+                properties: {
+                    name: `${c.state}, ${c.city}`,
+                },
+            },
+        });
+        documents.push(camp);
+    });
+
+    await CampgroundModel.bulkSave(documents);
+}
+db.init()
+    .then(seed)
+    .then(() => {
+        db.close();
+    })
+    .catch(() => {
+        console.log("Seeding complete!");
+    });
