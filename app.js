298d6e5f7d8c7d141d80f6da4ce318557bce3786
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-09-18 11:52:19 +0600

    feat: init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { db } = require("./db");
+const routes = require("./routes");
+const app = express();
+// app.set("view engine", nunjucks);
+//app.set("views", path.join(__dirname, "views"));
+db.init();
+app.use(routes);
+
+const server = app.listen(3000, () => {
+    console.log("Serving on 3000");
+});
+
+process.on("SIGTERM", () => {
+    console.info("SIGTERM signal received.");
+    console.log("Closing http server.");
+    db.close();
+    server.close((err) => {
+        console.log("Http server closed.");
+        process.exit(err ? 1 : 0);
+    });
+});
