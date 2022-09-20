a3275bf41366e98caf8b57f49a59fcf75afdfd6d
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-09-20 19:27:15 +0600

    feat: add method override and parsing middleware

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const morgan = require("morgan");
 const path = require("path");
 const { db } = require("./db");
 const routes = require("./routes");
+const methodOverride = require("method-override");
 const app = express();
 app.disable("x-powered-by");
 app.set("view engine", "pug");
 //app.set("views", path.join(__dirname, "views"));
 db.init();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(morgan("tiny"));
-
+app.use(methodOverride("_method"));
 app.use(routes);
 
 const server = app.listen(3000, () => {
