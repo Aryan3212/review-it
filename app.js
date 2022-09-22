c43715ea3582895f783a43a4880bd0ed5e45ddd6
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-09-22 15:51:20 +0600

    feat: added error fallback middleware and linting

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
-const { db } = require("./db");
-const routes = require("./routes");
-const methodOverride = require("method-override");
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const methodOverride = require('method-override');
+const { db } = require('./db');
+const routes = require('./routes');
+
 const app = express();
-app.disable("x-powered-by");
-app.set("view engine", "pug");
-//app.set("views", path.join(__dirname, "views"));
-db.init();
+
+app.disable('x-powered-by');
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/public", express.static(path.join(__dirname, "public")));
-app.use(morgan("tiny"));
-app.use(methodOverride("_method"));
+app.use(morgan('dev'));
+app.use(methodOverride('_method'));
+
+db.init();
+
 app.use(routes);
 
-const server = app.listen(3000, () => {
-    console.log("Serving on 3000");
+//* Error Routes
+app.use((err, req, res, next) => {
+  console.log('Caught!!', err);
+  res.render('error');
+  next();
 });
 
-process.on("SIGTERM", () => {
-    console.info("SIGTERM signal received.");
-    console.log("Closing http server.");
-    db.close();
-    server.close((err) => {
-        console.log("Http server closed.");
-        process.exit(err ? 1 : 0);
-    });
+const server = app.listen(3000, () => {
+  console.log('Serving on 3000');
+});
+
+process.on('SIGTERM', () => {
+  console.info('SIGTERM signal received.');
+  console.log('Closing http server.');
+  db.close();
+  server.close(err => {
+    console.log('Http server closed.');
+    process.exit(err ? 1 : 0);
+  });
 });
