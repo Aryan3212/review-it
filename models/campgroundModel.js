9b2a81686fa4d79edb7572952784bae795cd324b
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-09-22 15:56:18 +0600

    feat: add indexes for queries

--- models/campgroundModel.js
+++ models/campgroundModel.js
@@ -1,43 +1,54 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
 
 // Validator functions
 
 const coordinatesArrayValidator = (a) => a.length == 2;
 
 // Schema definitions
+const ImageSchema = new Schema({
+  url: String,
+  filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+  return this.url.replace('/upload', '/upload/w_200');
+});
 
 const LocationSchema = new Schema(
-    {
-        type: { type: String, enum: ["Feature"], default: "Feature" },
-        geometry: {
-            type: { type: String, enum: ["Point"], default: "Point" },
-            coordinates: {
-                type: [Number],
-                validate: {
-                    validator: coordinatesArrayValidator,
-                },
-            },
-        },
-        properties: {
-            name: String,
-        },
+  {
+    type: { type: String, enum: ['Feature'], default: 'Feature' },
+    geometry: {
+      type: { type: String, enum: ['Point'], default: 'Point' },
+      coordinates: {
+        type: [Number],
+        validate: {
+          validator: coordinatesArrayValidator
+        }
+      }
     },
-    { _id: false }
+    properties: {
+      name: String
+    }
+  },
+  { _id: false }
 );
 
 const CampgroundSchema = new Schema({
-    title: String,
-    price: {
-        type: Number,
-        min: 0,
-    },
-    description: String,
-    location: LocationSchema,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
+  title: String,
+  price: {
+    type: Number,
+    min: 0
+  },
+  description: String,
+  location: LocationSchema,
+  images: [ImageSchema],
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true
+  }
 });
 
-module.exports.CampgroundModel = mongoose.model("Campground", CampgroundSchema);
+module.exports.CampgroundModel = mongoose.model('Campground', CampgroundSchema);
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,23 +1,26 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
 
 // Schema definitions
 
 const ReviewSchema = new Schema({
-    details: String,
-    rating: {
-        type: Number,
-        min: 1,
-        max: 5,
-    },
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
-    campground: {
-        type: Schema.Types.ObjectId,
-        ref: "Campground",
-    },
+  details: String,
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true
+  },
+  campground: {
+    type: Schema.Types.ObjectId,
+    ref: 'Campground',
+    index: true
+  }
 });
 
-module.exports.CampgroundModel = mongoose.model("Review", ReviewSchema);
+module.exports.ReviewModel = mongoose.model('Review', ReviewSchema);
