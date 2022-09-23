340d35d00cbb335aab3ab03bd4c7d74604f81fe6
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-09-24 00:53:51 +0600

    feat: passport login route using async

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,15 +4,23 @@ const {
   loginUser,
   logoutUser,
   deleteUser,
-  updateUser,
+  updateUser
 } = require('../controllers/userController');
 const { catchAsync } = require('../utils');
-
+const passport = require('passport');
 const router = new Router();
 
 router.route('/').patch(catchAsync(updateUser)).delete(catchAsync(deleteUser));
 
-router.route('/login').post(catchAsync(loginUser));
+router.route('/login').post(
+  catchAsync(async (req, res, next) => {
+    passport.authenticate('local', {
+      successReturnToOrRedirect: '/',
+      failureRedirect: '/',
+      failureMessage: true
+    })(req, res, next);
+  })
+);
 router.route('/register').post(catchAsync(registerUser));
 router.route('/logout').post(catchAsync(logoutUser));
 
