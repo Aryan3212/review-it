907ad35bf6b2e2a92172c98e278badb6acc2c0ce
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-09-24 00:52:14 +0600

    feat: seed with authenticated users

--- seeds/index.js
+++ seeds/index.js
@@ -1,21 +1,28 @@
 const { CampgroundModel } = require('../models/campgroundModel');
-const { ReviewModel } = require('../models/ReviewModel');
-const { UserModel } = require('../models/UserModel');
+const { ReviewModel } = require('../models/reviewModel');
+const { UserModel } = require('../models/userModel');
 const { db } = require('../db');
 const camps = require('./cities').slice(0, 15);
 const { places, descriptors } = require('./titleDescriptors');
-
+require('dotenv').config();
 async function seed() {
   const documents = [];
   const reviews = [];
   await CampgroundModel.deleteMany({});
   await ReviewModel.deleteMany({});
   await UserModel.deleteMany({});
-  const user = UserModel({
+  const user = new UserModel({
+    username: 'aryan3212',
     email: 'rahman.aryan07@gmail.com'
   });
-  await user.save();
-  camps.forEach(c => {
+  await UserModel.register(user, '12345');
+  const user2 = new UserModel({
+    username: 'user3212',
+    email: 'user@gmail.com'
+  });
+  await UserModel.register(user2, '12345');
+  let toggle = true;
+  camps.forEach((c) => {
     const camp = new CampgroundModel({
       title: `${descriptors[Math.floor(Math.random() * descriptors.length)]} ${
         places[Math.floor(Math.random() * places.length)]
@@ -29,7 +36,8 @@ async function seed() {
         properties: {
           name: `${c.state}, ${c.city}`
         }
-      }
+      },
+      author: toggle ? user.id : user2.id
     });
     const review = new ReviewModel({
       rating: 4,
@@ -38,6 +46,28 @@ async function seed() {
       campground: camp.id
     });
     reviews.push(review);
+    const review2 = new ReviewModel({
+      rating: 4,
+      details: 'This is a great campground!',
+      author: user2.id,
+      campground: camp.id
+    });
+    reviews.push(review2);
+    const review3 = new ReviewModel({
+      rating: 4,
+      details: 'This is a great campground!',
+      author: user.id,
+      campground: camp.id
+    });
+    reviews.push(review3);
+    const review4 = new ReviewModel({
+      rating: 4,
+      details: 'This is a great campground!',
+      author: user2.id,
+      campground: camp.id
+    });
+    reviews.push(review4);
+    toggle = !toggle;
     documents.push(camp);
   });
 
@@ -50,7 +80,7 @@ db.init()
     db.close();
     console.log('Seeding complete!');
   })
-  .catch(e => {
+  .catch((e) => {
     db.close();
     console.log('An error happened!', e);
   });
