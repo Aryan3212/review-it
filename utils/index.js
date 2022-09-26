f253de806fe49f7794d1959049b7764f68e85b8e
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-09-26 13:47:06 +0600

    feat: image filter util

--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,9 @@
-module.exports.catchAsync = (fn) => (req, res, next) => fn(req, res, next).catch(next);
+module.exports.catchAsync = (fn) => (req, res, next) =>
+  fn(req, res, next).catch(next);
+module.exports.imageFilter = (req, file, cb) => {
+  // accept image files only
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+    return cb(new Error('Only image files are allowed!'), false);
+  }
+  cb(null, true);
+};
