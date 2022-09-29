a782538dabc173209a84bd445a8e2507c787dff1
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-09-30 04:25:40 +0600

    refactor: use recommended API remove unneeded code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 const {
   registerUser,
-  loginUser,
   logoutUser,
   deleteUser,
   updateUser
@@ -9,11 +8,10 @@ const {
 const { isAuthenticated } = require('../middleware/authMiddleware');
 const { catchAsync } = require('../utils');
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
 const { UserModel } = require('../models/userModel');
 const router = new Router();
 
-passport.use(new LocalStrategy(UserModel.authenticate()));
+passport.use(UserModel.createStrategy());
 
 passport.serializeUser(UserModel.serializeUser());
 passport.deserializeUser(UserModel.deserializeUser());
