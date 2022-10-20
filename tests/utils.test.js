d2f431ffdf290708a4b76020903b69b73b0339b1
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-10-20 21:23:42 +0600

    feat: utils unit tests

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,8 +1,6 @@
-const { catchAsync } = require('../utils/index.js');
+const { catchAsync, imageFilter } = require('../utils/index.js');
 
 describe('catching async functions work as intended', () => {
-  beforeEach(() => {});
-
   test('request is resolved', () => {
     const next = (err) => {
       if (err) return 'error';
@@ -32,6 +30,26 @@ describe('catching async functions work as intended', () => {
     const returnedFunction = catchAsync(dummyFunction);
     returnedFunction(req, res, next).then((value) => {
       expect(value).toBe('error');
+    });
+  });
+});
+
+describe('test image file format verifier', () => {
+  test('file format is an image and is verified', () => {
+    const fileNames = ['image.png', 'image.gif', 'image.jpeg', 'image.jpg'];
+    fileNames.forEach((name) => {
+      imageFilter({}, { originalname: name }, (err, value) => {
+        expect(value).toBe(true);
+      });
+    });
+  });
+
+  test('file format is not an image and is unverified', () => {
+    const fileNames = ['image.txt', 'image.docx', 'image.pdf', 'image.jpsga'];
+    fileNames.forEach((name) => {
+      imageFilter({}, { originalname: name }, (err, value) => {
+        expect(value).toBe(false);
+      });
     });
   });
 });
