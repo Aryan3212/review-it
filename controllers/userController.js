6c84c66b9bae6f2bb388a8a13e3972fa22426f99
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-11-02 15:11:33 +0600

    refactor(wip): user services and controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,40 +1,99 @@
-const { UserModel } = require('../models/userModel');
+const { isVerifiedEmail } = require('../services/emailVerificationService');
+const {
+    registerUserService,
+    loginUserService,
+    findUserService,
+    setUsernameAndPasswordService
+} = require('../services/userService');
 
 const logoutUser = async (req, res) => {
-  req.logout(() => {
-    res.redirect('/');
-  });
+    req.logout(() => {
+        // flash success
+        return res.redirect('/');
+    });
 };
 
 const registerUser = async (req, res, next) => {
-  const { username, email, password } = req.body;
-  const newUser = new UserModel({ username, email });
-  await UserModel.register(newUser, password, () => {
-    const authenticate = UserModel.authenticate();
-    authenticate(newUser.username, password, function (err, result) {
-      if (err) {
-        next(err);
-      }
-      req.login(result, (err) => {
+    const { username, email, password } = req.body;
+    const userWithEmailExists = await findUserService({ email });
+    if (userWithEmailExists) {
+        // flash error
+        return res.redirect('/');
+    }
+    const isValidEmail = await isVerifiedEmail(email);
+    if (!isValidEmail) {
+        // flash error
+        return res.redirect('/');
+    }
+    const newUser = await registerUserService({ username, email, password });
+
+    if (!newUser) {
+        // flash error
+        console.log('Registration failed');
+    }
+    const result = await loginUserService({ username, password });
+    req.login(result, (err) => {
         if (err) {
-          next(err);
+            // flash error
+            return next(err);
         }
-        res.redirect('/');
-      });
+        return res.redirect('/');
     });
-  });
 };
 
+const registerOAuthUser = async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!(username && password)) {
+        // flash error
+
+        return res.redirect('/users/oauth/finalize');
+    }
+    const returnedUser = await findUserService({ username });
+    if (returnedUser) {
+        // flash error
+        return res.redirect('/users/oauth/finalize');
+    }
+
+    const currentUser = req.user;
+    const currentUserToBeProcessed = await findUserService({
+        id: currentUser.id
+    });
+    await setUsernameAndPasswordService({
+        user: currentUserToBeProcessed,
+        username,
+        password
+    });
+    // flash success
+    return res.redirect('/');
+};
+
+const toggleVerifyUser = async (req, res) => {
+    const currentUser = req.user;
+    const newUser = UserModel.findById(currentUser.id);
+    newUser.verified = !newUser.verified;
+    // flash success
+    res.redirect('/');
+};
+const finalizeUser = async (req, res) => {
+    return res.status(200).render('auth/finalize');
+};
 const deleteUser = async (req, res) => {
-  // TODO
+    const { user } = req.user;
+    await UserModel.deleteOne({ id: user.id });
+    await req.logout();
+    // flash success
+    res.redirect('/');
 };
 
 const updateUser = async (req, res) => {
-  // TODO
+    const { username, password, email } = req.body;
 };
 module.exports = {
-  registerUser,
-  logoutUser,
-  updateUser,
-  deleteUser
+    registerUser,
+    logoutUser,
+    updateUser,
+    deleteUser,
+    finalizeUser,
+    registerOAuthUser
 };
---
+++ services/userService.js
@@ -0,0 +1,34 @@
+const { UserModel } = require('../models/userModel');
+const ObjectId = require('mongodb').ObjectId;
+const findUserService = async (query) => {
+    if ('id' in query) {
+        query['_id'] = ObjectId(query.id);
+        delete query.id;
+    }
+    const userFound = await UserModel.where(query).findOne();
+    return userFound;
+};
+
+const registerUserService = async ({ username, email }) => {
+    const userToBeRegistered = new UserModel({ username, email });
+    return await UserModel.register(newUser, password);
+};
+const loginUserService = async ({ username, password }) => {
+    const authenticate = await UserModel.authenticate();
+    return await authenticate(username, password);
+};
+const setUsernameAndPasswordService = async ({ user, username, password }) => {
+    await user.setPassword(password);
+    user.username = username;
+    user.verified = true;
+    await user.save();
+    const authenticate = UserModel.authenticate();
+    return await authenticate(user.username, password);
+};
+
+module.exports = {
+    findUserService,
+    registerUserService,
+    loginUserService,
+    setUsernameAndPasswordService
+};
