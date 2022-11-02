5c5f8047c569a73f3fb8ad5b0123c2b58b0bd5e8
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-11-02 15:12:54 +0600

    feat: middleware to limit user generated content

---
+++ middleware/limitItems.js
@@ -0,0 +1,30 @@
+const { PostModel } = require('../models/postModel');
+const { ReviewModel } = require('../models/ReviewModel');
+// Backend validator
+const limitReviews = async (req, res, next) => {
+  const currentUser = req.user;
+  const { post_id } = req.params;
+  const reviews = await ReviewModel.find({
+    author: currentUser.id,
+    post: post_id
+  });
+  if (reviews.length > 0) {
+    return res.redirect(`/posts/${post_id}`);
+  }
+  return next();
+};
+
+const limitPosts = async (req, res, next) => {
+  const currentUser = req.user;
+  const { id } = req.params;
+  const posts = await PostModel.find({ author: currentUser.id, id });
+  if (posts.length === 5) {
+    return res.redirect(`/`);
+  }
+  return next();
+};
+
+module.exports = {
+  limitPosts,
+  limitReviews
+};
