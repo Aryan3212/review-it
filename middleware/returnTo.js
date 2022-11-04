11341f99eab20145a49607eb64cf47027b47c9a3
Aryan Rahman <rahman.aryan07@gmail.com>
2022-11-04 19:10:24 +0600

    wip

--- middleware/returnTo.js
+++ middleware/returnTo.js
@@ -1,9 +1,9 @@
 const returnTo = async (req, res, next) => {
-  const currentUser = req.currentPath;
-  const { post_id } = req.params;
-  let reviews = await ReviewModel.find({ author: currentUser });
-  if (!post.author.equals(req.user.id)) {
-    return res.redirect(`/posts/${id}`);
-  }
-  return next();
+    const currentUser = req.currentPath;
+    const { post_id } = req.params;
+    let reviews = await ReviewModel.find({ author: currentUser });
+    if (!post.author.equals(req.user.id)) {
+        return res.redirect(`/posts/${id}`);
+    }
+    return next();
 };
