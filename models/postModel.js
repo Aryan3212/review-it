c472dda368189238f445a1226984482990f0969e
Aryan Rahman <rahman.aryan07@gmail.com>
2022-11-04 18:36:18 +0600

    feat: use strict query for models

--- models/postModel.js
+++ models/postModel.js
@@ -7,93 +7,100 @@ const coordinatesArrayValidator = (a) => a.length == 2;
 
 // Schema definitions
 const ImageSchema = new Schema({
-  url: {
-    type: String,
-    minLength: 1,
-    required: true,
-    default: process.env.DEFAULT_IMG
-  },
-  filename: {
-    type: String,
-    minLength: 1,
-    required: true
-  }
+    url: {
+        type: String,
+        minLength: 1,
+        required: true,
+        default: process.env.DEFAULT_IMG
+    },
+    filename: {
+        type: String,
+        minLength: 1,
+        required: true
+    }
 });
 
 ImageSchema.virtual('thumbnail').get(function () {
-  return this.url.replace('/upload', '/upload/w_200');
+    return this.url.replace('/upload', '/upload/w_200');
 });
 
 const opts = {
-  toJSON: { virtuals: true },
-  toObject: {
-    virtuals: true,
-    transform: (doc, ret, opts) => {
-      ret.id = ret._id.toString();
-      delete ret._id;
-      delete ret.__v;
-      return ret;
+    strict: true,
+    strictQuery: false,
+    toJSON: { virtuals: true },
+    toObject: {
+        virtuals: true,
+        transform: (doc, ret, opts) => {
+            ret.id = ret._id.toString();
+            delete ret._id;
+            delete ret.__v;
+            return ret;
+        }
     }
-  }
 };
 
 const LocationSchema = new Schema(
-  {
-    type: {
-      type: String,
-      enum: ['Feature'],
-      default: 'Feature',
-      required: true
-    },
-    geometry: {
-      type: { type: String, enum: ['Point'], default: 'Point', required: true },
-      coordinates: {
-        type: [Number],
-        validate: {
-          validator: coordinatesArrayValidator
+    {
+        type: {
+            type: String,
+            enum: ['Feature'],
+            default: 'Feature',
+            required: true
         },
-        required: true
-      }
+        geometry: {
+            type: {
+                type: String,
+                enum: ['Point'],
+                default: 'Point',
+                required: true
+            },
+            coordinates: {
+                type: [Number],
+                validate: {
+                    validator: coordinatesArrayValidator
+                },
+                required: true
+            }
+        },
+        properties: {
+            name: {
+                type: String,
+                minLength: 1,
+                maxLength: 300,
+                required: true
+            }
+        }
     },
-    properties: {
-      name: {
-        type: String,
-        minLength: 1,
-        maxLength: 300,
-        required: true
-      }
-    }
-  },
-  { _id: false }
+    { _id: false }
 );
 
 const PostSchema = new Schema(
-  {
-    title: {
-      type: String,
-      minLength: 1,
-      maxLength: 300
+    {
+        title: {
+            type: String,
+            minLength: 1,
+            maxLength: 300
+        },
+        price: {
+            type: Number,
+            min: 0
+        },
+        description: {
+            type: String,
+            minLength: 1,
+            maxLength: 1500
+        },
+        location: LocationSchema,
+        images: [ImageSchema],
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+            index: true
+        }
     },
-    price: {
-      type: Number,
-      min: 0
-    },
-    description: {
-      type: String,
-      minLength: 1,
-      maxLength: 1500
-    },
-    location: LocationSchema,
-    images: [ImageSchema],
-    author: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-      index: true
-    }
-  },
-  opts
+    opts
 );
 
 module.exports.PostModel =
-  mongoose.model.Post || mongoose.model('Post', PostSchema);
+    mongoose.model.Post || mongoose.model('Post', PostSchema);
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -2,47 +2,49 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const opts = {
-  toJSON: { virtuals: true },
-  toObject: {
-    virtuals: true,
-    transform: (doc, ret, opts) => {
-      ret.id = ret._id.toString();
-      delete ret._id;
-      delete ret.__v;
-      return ret;
+    strict: true,
+    strictQuery: false,
+    toJSON: { virtuals: true },
+    toObject: {
+        virtuals: true,
+        transform: (doc, ret, opts) => {
+            ret.id = ret._id.toString();
+            delete ret._id;
+            delete ret.__v;
+            return ret;
+        }
     }
-  }
 };
 
 // Schema definitions
 
 const ReviewSchema = new Schema(
-  {
-    details: {
-      type: String,
-      minLength: 1,
-      maxLength: 1500
+    {
+        details: {
+            type: String,
+            minLength: 1,
+            maxLength: 1500
+        },
+        rating: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            index: true,
+            required: true
+        },
+        post: {
+            type: Schema.Types.ObjectId,
+            ref: 'Post',
+            index: true,
+            required: true
+        }
     },
-    rating: {
-      type: Number,
-      min: 1,
-      max: 5
-    },
-    author: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      index: true,
-      required: true
-    },
-    post: {
-      type: Schema.Types.ObjectId,
-      ref: 'Post',
-      index: true,
-      required: true
-    }
-  },
-  opts
+    opts
 );
 
 module.exports.ReviewModel =
-  mongoose.model.Review || mongoose.model('Review', ReviewSchema);
+    mongoose.model.Review || mongoose.model('Review', ReviewSchema);
