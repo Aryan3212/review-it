8bdd15dc0026bd872fa24a6fd05ee1ecc954c925
Aryan Rahman <rahman.aryan07@gmail.com>
2022-11-04 19:06:34 +0600

    feat: verify email config

--- services/emailVerificationService.js
+++ services/emailVerificationService.js
@@ -1,12 +1,15 @@
 const isVerifiedEmail = async (email) => {
-    const response = await fetch(
-    `${process.env.EMAIL_VERIFICATION_API_BASE_URL}?api_key=${process.env.EMAIL_VERIFICATION_API_KEY}&email=${email}`
-  );
-  const verification = await response.json();
-  const isValid = verification.is_smtp_valid.value;
-  return isValid;
+    if (process.env.EMAIL_VERIFICATION_ENABLED) {
+        const response = await fetch(
+            `${process.env.EMAIL_VERIFICATION_API_BASE_URL}?api_key=${process.env.EMAIL_VERIFICATION_API_KEY}&email=${email}`
+        );
+        const verification = await response.json();
+        const isValid = verification.is_smtp_valid.value;
+        return isValid;
+    }
+    return true;
 };
 
 module.exports = {
-  isVerifiedEmail
+    isVerifiedEmail
 };
