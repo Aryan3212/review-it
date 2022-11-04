baba2c3fd781e3c1980a2091fa52d3dbddeb3c46
Aryan Rahman <rahman.aryan07@gmail.com>
2022-11-04 18:35:32 +0600

    chore: clear logs

--- middleware/userVerifiedMiddleware.js
+++ middleware/userVerifiedMiddleware.js
@@ -3,7 +3,6 @@ const { UserModel } = require('../models/userModel');
 const isVerified = async (req, res, next) => {
     const currentUser = req.user;
     const userData = await UserModel.findById(currentUser.id);
-
     if (userData.verified) {
         return next();
     }
@@ -13,7 +12,6 @@ const isVerified = async (req, res, next) => {
 const isNotVerified = async (req, res, next) => {
     const currentUser = req.user;
     const userData = await UserModel.findById(currentUser.id);
-    console.log(userData);
     if (!userData.verified) {
         return next();
     }
