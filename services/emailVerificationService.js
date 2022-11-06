80109054a1bf476d61b11d2ba425ad7f83980539
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-11-06 13:10:14 +0600

    fix: sanitize email and use env to enable verify

--- services/emailVerificationService.js
+++ services/emailVerificationService.js
@@ -1,7 +1,10 @@
+const { sanitizeEmail } = require('../utils');
+
 const isVerifiedEmail = async (email) => {
+    const cleanEmail = sanitizeEmail(email);
     if (process.env.EMAIL_VERIFICATION_ENABLED) {
         const response = await fetch(
-            `${process.env.EMAIL_VERIFICATION_API_BASE_URL}?api_key=${process.env.EMAIL_VERIFICATION_API_KEY}&email=${email}`
+            `${process.env.EMAIL_VERIFICATION_API_BASE_URL}?api_key=${process.env.EMAIL_VERIFICATION_API_KEY}&email=${cleanEmail}`
         );
         const verification = await response.json();
         const isValid = verification.is_smtp_valid.value;
