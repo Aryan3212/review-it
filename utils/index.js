d2820cf2a341be62595e5b14ce9ffa6d1506642a
Aryan Rahman <rahman.aryan07@gmail.com>
2022-11-06 23:32:03 +0600

    feat: allow webp format upload

--- utils/index.js
+++ utils/index.js
@@ -8,7 +8,7 @@ const catchAsync = (fn) => {
 
 const imageFilter = (req, file, cb) => {
     // accept image files only
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif|webp)$/i)) {
         return cb(new Error('Only image files are allowed!'), false);
     }
     return cb(null, true);
