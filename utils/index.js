eb2dfc3494cb5472a4818fc08ac8f5ee7574107e
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2022-11-06 13:11:46 +0600

    feat: sanitize field utils
    Also refactored to declare functions as local variables

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,12 @@
-module.exports.catchAsync = (fn) => {
+const { escape, normalizeEmail, stripLow, trim } = require('validator');
+
+const catchAsync = (fn) => {
     return (req, res, next) => {
         fn(req, res, next).catch(next);
     };
 };
 
-module.exports.imageFilter = (req, file, cb) => {
+const imageFilter = (req, file, cb) => {
     // accept image files only
     if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
         return cb(new Error('Only image files are allowed!'), false);
@@ -12,11 +14,39 @@ module.exports.imageFilter = (req, file, cb) => {
     return cb(null, true);
 };
 
-module.exports.processUploadedImageFiles = (files) => {
+const processUploadedImageFiles = (files) => {
     return files.map((file) => {
         return {
-            url: file.path,
-            filename: file.filename
+            url: this.sanitizeUserInput(file.path),
+            filename: this.sanitizeUserInput(file.filename)
         };
     });
+};
+const sanitizeEmail = (email) => {
+    return normalizeEmail(sanitizeUserInput(email));
+};
+const sanitizeUserInput = (input) => {
+    const trimmed = trim(input);
+    const stripped = stripLow(trimmed);
+    const escaped = escape(stripped);
+    return escaped;
+};
+const objectMap = (obj, fn) => {
+    return Object.fromEntries(
+        Object.entries(obj).map(([k, v], i) => [k, fn(v, k, i)])
+    );
+};
+
+const populateFields = (fieldsArray) => {
+    return fieldsArray.map((field)=> {
+        return {path: field}
+    })
+}
+module.exports = {
+    sanitizeEmail,
+    sanitizeUserInput,
+    objectMap,
+    processUploadedImageFiles,
+    imageFilter,
+    catchAsync
 };
