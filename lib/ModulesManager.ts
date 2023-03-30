30001f84a969685fe7e582482dacf05a7f4366cd
Aryan Rahman <rahman.aryan07@gmail.com>
2023-03-31 05:22:41 +0600

    chore: fix lib types

---
+++ lib/ModulesManager.ts
@@ -0,0 +1,17 @@
+const EventEmitter = require("events");
+
+type ModuleObject = {
+    initialize: Function
+}
+
+class ModulesManager extends EventEmitter {
+    constructor(app: Object) {
+        super();
+        this.app = app;
+    }
+    
+    inject(module: ModuleObject) {
+      	module.initialize();
+        this.app.use(module);
+    }
+}
